edae298bc2c86e26e7a2ee8574a5322d3568418e
Nelsibengono <Nelsibengono@gmail.com>
2026-07-18 11:30:34 +0100

    Configure Astro for Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://Nelsibengono.github.io',
-    base: '/georges-I',
+  site: 'https://georges-i.vercel.app',
 });
